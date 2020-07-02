5654e045511491d5c47799665eee6df4f93046c8
lynnnakiganda <lynnnakigandasekajugo@gmail.com>
2020-07-02 15:26:55 +0300

    added uganda towns layer

--- js/side_panel_html.js
+++ js/side_panel_html.js
@@ -13,7 +13,7 @@ let african_sidepanel_text = {
 
 let ugandan_sidepanel_text = {
     "Infrastructure": [
-        "Border Points", "Health Referral Centers", "ICU Beds Per Health Center",
+        "Towns", "Border Points", "Health Referral Centers", "ICU Beds Per Health Center",
     ],
     "Districts Data": [
         "Border Districts", "Cases per District", "Poverty Percentage", "Population Density", "Contacts",
